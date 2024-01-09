7f4bc3b350ba1c945da03e7b04bf8966d86dd7ee
董浩 <2281501756@qq.com>
2024-01-09 20:05:15 +0800

    pstree begin

---
+++ cless1/m1/pstree.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<assert.h>
+
+int main(int argc, char *argv[]) {
+    printf("%d \n", argc);
+    for (int i = 0; i < argc; i ++) {
+        assert(argv[i]);
+        printf("argv[%d] = %s \n", i, argv[i]);
+    }
+    assert(!argv[argc]);
+    return 0;
+}
