39f6785c3a0d754b0475a389b0302236d0b41a76
董浩 <2281501756@qq.com>
2024-01-13 18:58:33 +0800

    commit go

--- cless1/m1/go/main.go
+++ cless1/m1/go/main.go
@@ -1,7 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+)
 
+func main() {
+	filepath.WalkDir("/proc", func(path string, d fs.DirEntry, err error) error {
+		// if d.IsDir() && unicode.IsDigit(rune(d.Name()[0])) {
+		// }
+		if d == nil {
+			return nil
+		}
+		fmt.Println(d.Name())
+		return nil
+	})
 
-func main()  {
-	filepatch	
 }
